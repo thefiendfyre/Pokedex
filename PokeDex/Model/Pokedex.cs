b76356428ff5f6a3eb56a057fe4ca09b58fa5e91
thefiendfyre <ddoremu1@my.westga.edu>
2016-05-08 01:52:44 -0400

    Added Pokedex class.

---
+++ PokeDex/Model/Pokedex.cs
@@ -0,0 +1,15 @@
+﻿namespace PokeDex.Model
+{
+    /// <summary>
+    /// Holds data for the Pokedex class.
+    /// </summary>
+    /// <version>
+    /// Summer 2016
+    /// </version>
+    /// <author>
+    /// Danl Doremus
+    /// </author>
+    internal class Pokedex
+    {
+    }
+}
