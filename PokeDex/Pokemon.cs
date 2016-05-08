57b8c3b86543a941dd3e50916c14b0f89e2aa680
thefiendfyre <ddoremu1@my.westga.edu>
2016-05-07 22:02:33 -0400

    Init commit.

---
+++ PokeDex/Pokemon.cs
@@ -0,0 +1,44 @@
+﻿namespace PokeDex
+{
+    /// <summary>
+    /// Holds data for the Pokemon class.
+    /// </summary>
+    /// <author>
+    /// Danl Doremus
+    /// </author>
+    /// <version>
+    /// Summer 2016
+    /// </version>
+    internal class Pokemon
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the height.
+        /// </summary>
+        /// <value>
+        /// The height.
+        /// </value>
+        public double Height { get; set; }
+
+        /// <summary>
+        /// Gets or sets the weight.
+        /// </summary>
+        /// <value>
+        /// The weight.
+        /// </value>
+        public double Weight { get; set; }
+
+        /// <summary>
+        /// Gets or sets the pokedex number.
+        /// </summary>
+        /// <value>
+        /// The pokedex number.
+        /// </value>
+        public int PokedexNumber { get; set; }  
+
+        
+
+        #endregion
+    }
+}
